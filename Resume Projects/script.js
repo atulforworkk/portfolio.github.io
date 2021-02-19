9088d430bc66a64261783c8e492fc42b27e3f35c
atulforworkk <76222032+atulforworkk@users.noreply.github.com>
2021-02-19 22:23:55 +0530

    My Portfolio

---
+++ Resume Projects/script.js
@@ -0,0 +1,11 @@
+// const url = 'https://jsonmock.hackerrank.com/api/movies?year'
+
+// const fetchApi = async (year)=>{
+// const result  = await fetch('https://jsonmock.hackerrank.com/api/movies?year=2015')
+// console.log(result)
+// const data = await result.json()
+// console.log(data)
+// }
+const el  = document.getElementById('test')
+console.log(el)
+// fetchApi(2015)
